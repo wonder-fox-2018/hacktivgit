734b06a14d83dfc2d95aa9893f92cf882549f9b4
esadeli <efrat.sadeli@gmail.com>
2018-10-03 12:47:25 +0700

    Get List of Repository by username

--- controllers/RepoController.js
+++ controllers/RepoController.js
@@ -5,7 +5,7 @@ const request = require('request')
 class RepoController{
 
     // get list of starred repo
-    static getListOfRepo(req,res){
+    static getListOfStarredRepo(req,res){
         request({
             url: `https://api.github.com/user/starred`,
             headers:{
@@ -16,6 +16,28 @@ class RepoController{
             if(!err){
                 res.status(200).json({
                     msg: 'List of Repo',
+                    data: JSON.parse(body)
+                })
+            }else{
+                res.status(500).json({
+                    msg: 'ERROR: ',err
+                })
+            }
+        })
+    }
+
+    // get repository by username
+    static getRepositoryByUsername(req,res){
+        request({
+            url: `https://api.github.com/users/${req.body.username}/repos`,
+            headers:{
+                'User-Agent': 'request',
+                Authorization: 'token '+process.env.GITTOKEN
+            }
+        },(err, response,body) =>{
+            if(!err){
+                res.status(200).json({
+                    msg: `List of repos from ${req.body.username}`,
                     data: JSON.parse(body)
                 })
             }else{
--- routes/RepoRouter.js
+++ routes/RepoRouter.js
@@ -4,8 +4,19 @@ const express = require('express')
 const router = express.Router()
 const RepoController = require('../controllers/RepoController')
 
+// get list of starred repo
 router.get('/lists', (req,res) =>{
-    RepoController.getListOfRepo(req,res)
+    RepoController.getListOfStarredRepo(req,res)
+})
+
+// get repository by username
+router.post('/username', (req,res) =>{
+    RepoController.getRepositoryByUsername(req,res)
+})
+
+// create repository
+router.post('/', (req,res) =>{
+
 })
 
 module.exports = router
